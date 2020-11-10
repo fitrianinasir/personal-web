69cb9117b72522fda193a79cb378d78d47e45ff9
fitrianinasir <fitrianinasir8@gmail.com>
2020-11-10 18:16:12 +0800

    about done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,49 @@
-import React from "react";
+import React, { useState, useEffect, useMemo } from "react";
 import Logo from "./assets/LOGO.jpeg";
 import IMG from "./assets/img.svg";
-import { Navbar, Nav, Container, Image, Jumbotron } from "react-bootstrap";
+import { Navbar, Nav, Container } from "react-bootstrap";
+import { useTransition, a } from "react-spring";
+import shuffle from "lodash/shuffle";
+import useMeasure from "./useMeasure";
+import useMedia from "./useMedia";
+import images from "./images";
+import "./App.sass";
 
 function App() {
+  // Hook1: Tie media queries to the number of columns
+  const columns = useMedia(
+    ["(min-width: 1500px)", "(min-width: 1000px)", "(min-width: 600px)"],
+    [5, 4, 3],
+    2
+  );
+  // Hook2: Measure the width of the container element
+  const [bind, { width }] = useMeasure();
+  // Hook3: Hold items
+  const [items, set] = useState(images);
+  // Hook4: shuffle data every 2 seconds
+  useEffect(() => void setInterval(() => set(shuffle), 3000), []);
+  // Hook5: Form a grid of stacked items using width & columns we got from hooks 1 & 2
+  const [heights, gridItems] = useMemo(() => {
+    let heights = new Array(columns).fill(0); // Each column gets a height starting with zero
+    let gridItems = items.map((child, i) => {
+      const column = heights.indexOf(Math.min(...heights)); // Basic masonry-grid placing, puts tile into the smallest column using Math.min
+      const xy = [
+        (width / columns) * column,
+        (heights[column] += child.height / 2) - child.height / 2,
+      ]; // X = container width / number of columns * column index, Y = it's just the height of the current column
+      return { ...child, xy, width: width / columns, height: child.height / 2 };
+    });
+    return [heights, gridItems];
+  }, [columns, items, width]);
+  // Hook6: Turn the static grid values into animated transitions, any addition, removal or change will be animated
+  const transitions = useTransition(gridItems, (item) => item.css, {
+    from: ({ xy, width, height }) => ({ xy, width, height, opacity: 0 }),
+    enter: ({ xy, width, height }) => ({ xy, width, height, opacity: 1 }),
+    update: ({ xy, width, height }) => ({ xy, width, height }),
+    leave: { height: 0, opacity: 0 },
+    config: { mass: 5, tension: 500, friction: 100 },
+    trail: 25,
+  });
   const jumbotronStyle = {
     backgroundImage: `url(${IMG})`,
     minHeight: "750px",
@@ -13,18 +53,6 @@ function App() {
     backgroundSize: "cover",
   };
 
-  const aboutStyle = {
-    height: "50em ",
-    width: "100%",
-    backgroundColor: "#0F222D",
-  };
-
-  const aboutText = {
-    width: "25rem",
-    height: "25rem",
-    backgroundColor: "white",
-    display: "flex",
-  };
   return (
     <div className="App">
       <Navbar>
@@ -36,7 +64,7 @@ function App() {
             <Nav.Link className="text-dark" href="#home">
               Home
             </Nav.Link>
-            <Nav.Link className="text-dark" href="#features">
+            <Nav.Link className="text-dark" href="#about">
               About
             </Nav.Link>
             <Nav.Link className="text-dark" href="#pricing">
@@ -48,16 +76,44 @@ function App() {
           </Nav>
         </Container>
       </Navbar>
-      <div style={jumbotronStyle}>
-        <Container >
-          <div style={{marginTop:'15rem', display:'inline-block'}}>
+      <div id="home" style={jumbotronStyle}>
+        <Container>
+          <div style={{ marginTop: "15rem", display: "inline-block" }}>
             <h1>Hello. I'm Fitriani Nasir</h1>
-            <h3>A Web Enthusiast</h3>
-            <div style={{}}>CONTACT ME</div>
+            <h2 style={{ fontFamily: "Caveat Brush" }}>A Web Enthusiast</h2>
+            <br />
+            <a href="#" class="cta">
+              <span>Curriculum Vitae</span>
+              <svg width="13px" height="10px" viewBox="0 0 13 10"></svg>
+            </a>
           </div>
         </Container>
       </div>
-      <div style={aboutStyle}>{/* <div style={aboutText}></div> */}</div>
+      <Container id="about">
+        <div {...bind} class="list" style={{ height: Math.max(...heights) }}>
+          {transitions.map(({ item, props: { xy, ...rest }, key }) => (
+            <a.div
+              key={key}
+              style={{
+                transform: xy.interpolate(
+                  (x, y) => `translate3d(${x}px,${y}px,0)`
+                ),
+                ...rest,
+              }}
+            >
+              <div style={{ backgroundImage: item.css }} />
+            </a.div>
+          ))}
+        </div>
+        <p className="about-text">
+          I am a final-year student in the Department of Informatics Engineering
+          at Hasanuddin University. I am interested in Website Development, Data
+          Science, and other technology areas. I am currently a lead of one of
+          the global communities powered by Google Developer, namely DSC or
+          Developer Student Clubs Chapter Hasanuddin University.
+        </p>
+      </Container>
+      <Container style={{ height: "30rem" }}></Container>
     </div>
   );
 }
---
+++ src/images.js
@@ -0,0 +1,43 @@
+export default [
+  {
+    css: "url(https://i.ibb.co/j4rc0gK/me.jpg)",
+    height: 300,
+  },
+
+  {
+    css: "url(https://i.ibb.co/85km9bX/group.jpg",
+    height: 300,
+  },
+  {
+    css: "url(https://i.ibb.co/CQ0ZrT6/DSCF5012.jpg)",
+    height: 300,
+  },
+  {
+    css: "url(https://i.ibb.co/JdWf466/DSC00103.jpg)",
+    height: 300,
+  },
+  {
+    css: "url(https://i.ibb.co/R4fqBzN/coder.jpg)",
+    height: 300,
+  },
+  {
+    css: "url(https://i.ibb.co/G2GpC0g/IMG-20191201-192720-848.jpg)",
+    height: 300,
+  },
+  {
+    css: "url(https://i.ibb.co/mG1x5pq/mentor.jpg)",
+    height: 300,
+  },
+  {
+    css: "url(https://i.ibb.co/FnqpFvw/IMG-20200218-220750.jpg)",
+    height: 300,
+  },
+  {
+    css: "url(https://i.ibb.co/dgqbqFD/FB-IMG-1583769889270-01.jpg)",
+    height: 300,
+  },
+  {
+    css: "url(https://i.ibb.co/hD0wjn7/IMG-2382.jpg)",
+    height: 300,
+  },
+];
